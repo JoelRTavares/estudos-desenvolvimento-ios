90d72a466a07b79cd63553db191ecd36908b8673
Joel <joelrtavares2017@gmail.com>
2025-06-24 13:24:52 -0300

    Atualizando o projeto e adicionando a navegacao

---
+++ Desafios degreed/usingUIKit/usingUIKit/Services/MovieService.swift
@@ -0,0 +1,66 @@
+//
+//  MovieService.swift
+//  withUIKit
+//
+//  Created by Joel Tavares on 20/06/25.
+//
+
+
+import Foundation
+
+protocol MovieServiceProtocol {
+    func fetchUpcomingMovies() async throws -> [MovieDTO]
+    func fetchGenres() async throws -> [GenreDTO]
+    func fetchCast(movieId: Int) async throws -> [ActorDTO]
+    func fetchPhotos(movieId: Int) async throws -> [ImageDTO]
+}
+
+final class MovieService: MovieServiceProtocol {
+    private let apiKey: String
+    private let baseURL = "https://api.themoviedb.org/3"
+    
+    init(apiKey: String) {
+        self.apiKey = apiKey
+    }
+    
+    // 1. Requisição para filmes (agora retorna DTO, não o Model final)
+    func fetchUpcomingMovies() async throws -> [MovieDTO] {
+        let endpoint = "\(baseURL)/movie/upcoming?api_key=\(apiKey)"
+        let data = try await performRequest(endpoint: endpoint)
+        let response = try JSONDecoder().decode(MovieResponseDTO.self, from: data)
+        return response.results
+    }
+    
+    // 2. Requisição para gêneros
+    func fetchGenres() async throws -> [GenreDTO] {
+        let endpoint = "\(baseURL)/genre/movie/list?api_key=\(apiKey)"
+        let data = try await performRequest(endpoint: endpoint)
+        let response = try JSONDecoder().decode(GenreResponseDTO.self, from: data)
+        return response.genres
+    }
+    
+    // 3. Requisição para elenco
+    func fetchCast(movieId: Int) async throws -> [ActorDTO] {
+        let endpoint = "\(baseURL)/movie/\(movieId)/credits?api_key=\(apiKey)"
+        let data = try await performRequest(endpoint: endpoint)
+        let response = try JSONDecoder().decode(CastResponseDTO.self, from: data)
+        return response.cast
+    }
+    
+    func fetchPhotos(movieId: Int) async throws -> [ImageDTO] {
+        let endpoint = "\(baseURL)/movie/\(movieId)/images?api_key=\(apiKey)"
+        let data = try await performRequest(endpoint: endpoint)
+        let response = try JSONDecoder().decode(PhotosResponseDTO.self, from: data)
+        return response.backdrops
+    }
+
+    private func performRequest(endpoint: String) async throws -> Data {
+        guard let url = URL(string: endpoint) else { throw MovieError.InvalidUrl }
+        let (data, response) = try await URLSession.shared.data(from: url)
+        guard let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode == 200 else {
+            throw MovieError.InvalidResponse
+        }
+        return data
+    }
+}
